c40d76a3eb90dd5b9288ca680ccde30aabbbb7b4
Pavel Olizko <pavelolizko@Pavels-MacBook-Air.local>
2023-12-06 15:42:16 +0100

    Refactored, implemented [Random by author] and cleaned up.

--- main.py
+++ main.py
@@ -12,12 +12,13 @@ from telegram.ext import (
     ApplicationBuilder,
     CommandHandler,
     ContextTypes,
-    CallbackQueryHandler
+    CallbackQueryHandler,
+    ConversationHandler
 )
 
 
 API_URL = "https://api.quotable.io"
-DEFAULT_STATE, CHOOSING_COMMAND, TYPING_TAG  = range(3)
+DEFAULT_STATE, CHOOSING_COMMAND, TYPING_AUTHOR  = range(3)
 COMMANDS = [
     'help'
     'random',
@@ -49,28 +50,23 @@ logger = logging.getLogger(__name__)
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE, query=None):
     """Executes on /start."""
     print(f'User {update.effective_chat.effective_name} just used a /start command.')
-    global state 
-    state = CHOOSING_COMMAND
     if query:
         await query.edit_message_text('I am a bot, whose sole purpose is to send you quotes.\n\nAt the moment, only these commands are supported:', reply_markup=COMMANDS_KEYBOARD_MARKUP)
-        return
-    await update.message.reply_text('I am a bot, whose sole purpose is to send you quotes.\n\nAt the moment, only these commands are supported:', reply_markup=COMMANDS_KEYBOARD_MARKUP)
+    else:
+       await update.message.reply_text('I am a bot, whose sole purpose is to send you quotes.\n\nAt the moment, only these commands are supported:', reply_markup=COMMANDS_KEYBOARD_MARKUP)
 
 
 async def menu(update: Update, context: ContextTypes.DEFAULT_TYPE, query=None):
     """Menu to list all the available commands"""
-    global state
-    state = CHOOSING_COMMAND
     if query:
         await query.edit_message_text("This is a list of all available commands:\n\n<i>Tip: you don't have to use this menu, just type \"/\".</i>", parse_mode='HTML', reply_markup=COMMANDS_KEYBOARD_MARKUP)
-        return
-    await update.message.reply_text("This is a list of all available commands:\n\n<i>Tip: you don't have to use this menu, just type \"/\".</i>", parse_mode='HTML', reply_markup=COMMANDS_KEYBOARD_MARKUP)
+    else:
+        await update.message.reply_text("This is a list of all available commands:\n\n<i>Tip: you don't have to use this menu, just type \"/\".</i>", parse_mode='HTML', reply_markup=COMMANDS_KEYBOARD_MARKUP)
 
 
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE, query=None):
     """Displays a help message."""
     global state
-    state = CHOOSING_COMMAND
     if query:
         await query.edit_message_text(f"Hi, {update.effective_chat.first_name}, I am a bot!\n\nI can help you pick a quote. Type /menu for a list of available commands.")
         return
@@ -78,22 +74,20 @@ async def help(update: Update, context: ContextTypes.DEFAULT_TYPE, query=None):
 
 
 async def menu_buttons(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    global state
-    if state == CHOOSING_COMMAND:
-        state = DEFAULT_STATE
-        query = update.callback_query
-        await query.answer()
-        
-        if query.data == 'random':
-            await random(update=update, context=context, query=query)
-        if query.data == 'random_by_author':
-            await random_by_author(update=update, context=context, query=query)
-        elif query.data == 'menu':
-            await menu(update=update, context=context, query=query)
-        elif query.data == 'start':
-            await start(update=update, context=context, query=query)
-        elif query.data == 'help':
-            await help(update=update, context=context, query=query)
+    query = update.callback_query
+    await query.answer()
+    
+    if query.data == 'random':
+        await random(update=update, context=context, query=query)
+    if query.data == 'random_by_author':
+        await random_by_author(update=update, context=context, query=query)
+        return TYPING_AUTHOR
+    elif query.data == 'menu':
+        await menu(update=update, context=context, query=query)
+    elif query.data == 'start':
+        await start(update=update, context=context, query=query)
+    elif query.data == 'help':
+        await help(update=update, context=context, query=query)
 
 
 async def random(update: Update, context: ContextTypes.DEFAULT_TYPE, query=None):
@@ -103,9 +97,31 @@ async def random(update: Update, context: ContextTypes.DEFAULT_TYPE, query=None)
         return
     await context.bot.send_message(chat_id=update.effective_chat.id, text=quote)
 
+
+async def random_by_author(update: Update, context: ContextTypes.DEFAULT_TYPE, author=None, query=None):
+    if query:
+        await query.edit_message_text(text='Please enter author\'s name:')
+    else:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text='Please enter author\'s name:')
+    return TYPING_AUTHOR
+
+
+async def typing_author(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    author = update.message.text
+    quote = get_random_quote_by_author(author)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=quote)
+    return ConversationHandler.END
+
+
 # If none of the above were provided
 async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I don't recognize this command.")
+    
+    
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Canceled.")
+    return ConversationHandler.END
+
 
 # LOGIC
 def get_tags(message: str) -> list:
@@ -123,6 +139,18 @@ def get_random_quote() -> str:
     return f"{quote}\n\n- {author}"
 
 
+def get_random_quote_by_author(author: str) -> str:
+    """Returns a random quote by a specific author."""
+    author_format = author.capitalize().replace(" ", "-").replace(".", "")
+    response = requests.get(API_URL + f"/quotes/random?author={author_format}")
+    if len(response.json()) > 0:  
+        quote = '"' + response.json()[0]["content"] + '"'
+        author_source = response.json()[0]["author"]
+        return f"{quote}\n\n- {author_source}"
+    else:
+        return "I'm sorry, no quotes by this author were found."
+
+
 def main() -> None:
     """Start the bot."""
     application = ApplicationBuilder().token('6973604569:AAH2NChCYageGr2sbm0mTfeKResrn5ePVxY').build()
@@ -130,17 +158,37 @@ def main() -> None:
     start_handler = CommandHandler('start', start)
     menu_handler = CommandHandler('menu', menu)
     random_handler = CommandHandler('random', random)
+    random_by_author_handler = CommandHandler('random_by_author', random_by_author)
     help_handler = CommandHandler('help', help)
+    cancel_handler = CommandHandler('cancel', cancel)
+    author_handler = MessageHandler(filters.TEXT, typing_author)
     unknown_handler = MessageHandler(filters.ALL, unknown)
     menu_buttons_handler = CallbackQueryHandler(menu_buttons)
-
+    
+    conv_handler = ConversationHandler(
+        entry_points=[
+            start_handler,
+            random_by_author_handler,
+            menu_buttons_handler
+        ],
+        states={
+            TYPING_AUTHOR: [
+                cancel_handler, 
+                author_handler
+            ]
+        },
+        fallbacks=[cancel_handler]
+    )
+    
     application.add_handlers([
+        conv_handler,
+        menu_buttons_handler,
         start_handler, 
         menu_handler,
         random_handler,
+        random_by_author_handler,
         help_handler,
-        unknown_handler,
-        menu_buttons_handler,
+        unknown_handler
     ])
     
     application.run_polling()
